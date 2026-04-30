c10f2f6ddea3b5bdba5e0c8c7072c0cabd33a4be
Jean Lucas <jl.jean13@gmail.com>
2026-04-30 20:17:38 -0300

    Clean: remove nested folder

--- nata-system/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://jolclylflnqinweixrcy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvbGNseWxmbG5xaW53ZWl4cmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MDM2NjIsImV4cCI6MjA5Mjk3OTY2Mn0.DO8N367F0sj5j-qghlOg90ZYBtFaeGTpHT_iNynwaRg';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
